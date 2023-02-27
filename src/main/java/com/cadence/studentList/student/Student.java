34587b39494d6a041a29ab6dadc98370ad1983ce
alice-njf <najafialice@gmail.com>
2023-02-27 02:01:59 +0100

    v3

--- src/main/java/com/cadence/studentList/student/Student.java
+++ src/main/java/com/cadence/studentList/student/Student.java
@@ -1,5 +1,11 @@
 package com.cadence.studentList.student;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,9 +19,18 @@ import lombok.ToString;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Entity
+@Table
 public class Student {
-
+	@Id
+	@SequenceGenerator(
+			name = "student_sequence",
+			sequenceName = "student_sequence",
+			allocationSize = 1
+	)
+	@GeneratedValue(
+			generator = "student_sequence",
+			strategy = GenerationType.SEQUENCE)
 	//attributes
 	private Long id;
 	private String name;
